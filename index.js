a1dbf03d9d3220143f7c6c9ef843cebc95963d24
AbdulazizDelvan <86203683+AbdulazizDelvan@users.noreply.github.com>
2021-06-25 22:14:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+    function ele(){
+    var butBr = document.querySelector("button").remove();
+    var butIk= document.querySelector("#nameSurname").textContent="Abdulaziz Delvan";
+    let butuc= document.querySelector("#age").textContent="23";
+    }
+    var aBr=document.querySelector('#linkedinUrl')
+    aBr.addEventListener('click', event =>
+    event.target.href ="https://www.linkedin.com/in/abdulaziz-delvan-7b54bb215/");
+    var aIK=document.querySelector("#githubUrl")
+    aIK.addEventListener('click', event =>
+    event.target.href ="https://github.com/AbdulazizDelvan");
